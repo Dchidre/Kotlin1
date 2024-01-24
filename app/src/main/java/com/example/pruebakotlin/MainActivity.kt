34c7792d190d308c1a943ab5c52ed3a22fc6a304
chircea <david.chircea@gmail.com>
2024-01-24 10:50:41 +0100

    Start of courses (https://developer.android.com/codelabs/basic-android-kotlin-compose-text-composables?authuser=1&continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-compose-unit-1-pathway-3%3Fauthuser%3D1%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-compose-text-composables#5)

---
+++ app/src/main/java/com/example/pruebakotlin/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.pruebakotlin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.pruebakotlin.ui.theme.PruebaKotlinTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PruebaKotlinTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingText(modifier: Modifier = Modifier, message: String) {
+    Text(text = message)
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PruebaKotlinTheme {
+        GreetingText(message = "Felicidades hostia!")
+    }
+}
