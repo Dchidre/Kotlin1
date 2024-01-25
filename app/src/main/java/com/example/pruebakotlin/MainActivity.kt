d35a25af939406af47b9cc8a2129608ec6c5c03f
chircea <david.chircea@gmail.com>
2024-01-25 02:08:32 +0100

    image added

--- app/src/main/java/com/example/pruebakotlin/MainActivity.kt
+++ app/src/main/java/com/example/pruebakotlin/MainActivity.kt
@@ -3,16 +3,25 @@ package com.example.pruebakotlin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.pruebakotlin.ui.theme.PruebaKotlinTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,20 +29,57 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PruebaKotlinTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                }
+                GreetingImage(stringResource(R.string.birth_text), stringResource(R.string.signature))
             }
         }
     }
 }
 
 @Composable
-fun GreetingText(modifier: Modifier = Modifier, message: String) {
-    Text(text = message)
+fun GreetingText(modifier: Modifier = Modifier, message: String, from: String) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 110.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.W400,
+            fontStyle = FontStyle.Italic,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = modifier,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(1.dp)
+        )
+    }
+
 }
 
 
@@ -41,6 +87,6 @@ fun GreetingText(modifier: Modifier = Modifier, message: String) {
 @Composable
 fun GreetingPreview() {
     PruebaKotlinTheme {
-        GreetingText(message = "Felicidades hostia!")
+        GreetingImage(stringResource(R.string.birth_text), stringResource(R.string.signature))
     }
 }
